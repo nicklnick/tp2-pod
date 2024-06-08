d1bff4225c6ef9053febca795f0d121842c81f0b
Elian Paredes <elianparedes.dev@gmail.com>
2024-06-07 22:25:04 -0300

    chore: csv file writer implementation

---
+++ client/src/main/java/ar/edu/itba/pod/hazelcast/file/CsvFileWriter.java
@@ -0,0 +1,28 @@
+package ar.edu.itba.pod.hazelcast.file;
+
+import com.opencsv.CSVWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class CsvFileWriter {
+
+    private static final char SEPARATOR = ';';
+
+    private CsvFileWriter() {
+        throw new AssertionError("This class should not be instantiated");
+    }
+
+    public static void writeRows(String filePath, List<String[]> rows) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(filePath), SEPARATOR,
+                CSVWriter.NO_QUOTE_CHARACTER,
+                CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                CSVWriter.DEFAULT_LINE_END)) {
+            for (String[] row : rows) {
+                writer.writeNext(row);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
